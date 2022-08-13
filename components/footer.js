cfed29db4a69a32a6bee025bdd95742628a72aaa
Christina <christinaraganit@gmail.com>
2022-08-12 23:33:53 -0700

    some ui updates

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,7 @@
 export default function Footer() {
   return (
-    <div className="mb-10 px-10 text-center font-mono text-md text-zinc-400">
-      <p>Developed by Christina Raganit ⭐</p>
-      <p>Built for Eric Dam 🍚</p>
+    <div className="mb-5 px-10 text-center font-mono text-md text-zinc-400">
+      <p>Developed by Eric Dam 🍚</p>
     </div>
   );
 }
--- components/header.js
+++ components/header.js
@@ -24,41 +24,61 @@ export default function Header() {
       // links.classList.add("hidden");
       setOpen(false);
     }
-
-  }
+  };
 
   return (
-    <div
-      id="header"
-      className="top-0 h-20 z-100 w-full fixed bg-zinc-900/40 border-b-2 border-b-zinc-800"
-    >
-      <div className="p-5 flex flex-col items-end backdrop-blur-lg">
-        <HiOutlineMenuAlt3 size={36} className="text-purple-200" onClick={() => openMenu()}></HiOutlineMenuAlt3>
+    <div id="header" className="top-0 h-20 z-100 w-full fixed">
+      <div className="p-5 flex flex-col items-end backdrop-blur-lg border-b-2 border-b-zinc-800">
+        <HiOutlineMenuAlt3
+          size={36}
+          className="text-purple-200"
+          onClick={() => openMenu()}
+        ></HiOutlineMenuAlt3>
       </div>
 
-      <div id="mobile-menu" className="top-0 fixed z-101 text-purple-100 text-l font-semibold top-0 h-0 w-full bg-zinc-900 transition-all ease-in-out duration-300 overflow-hidden">
-        <div id="mobile-menu-links" className="h-full flex gap-3 flex-col p-5 items-center">
-          <IoMdClose size={36} className="text-purple-200 self-end mb-5" onClick={() => openMenu()}></IoMdClose>
+      <div
+        id="mobile-menu"
+        className="top-0 fixed z-101 text-purple-100 text-l font-semibold top-0 h-0 w-full bg-zinc-900 transition-all ease-in-out duration-300 overflow-hidden"
+      >
+        <div
+          id="mobile-menu-links"
+          className="h-full flex gap-3 flex-col p-5 items-center"
+        >
+          <IoMdClose
+            size={36}
+            className="text-purple-200 self-end mb-5"
+            onClick={() => openMenu()}
+          ></IoMdClose>
           <div className="flex gap-5 flex-col items-center">
-            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">About me</a>
-            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">Featured projects</a>
-            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">All projects</a>
-            <a className="p-4 w-full text-center bg-purple-700 hover:bg-purple-800 rounded-md">Resume</a>
+            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">
+              About me
+            </a>
+            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">
+              Featured projects
+            </a>
+            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">
+              All projects
+            </a>
+            <a className="p-4 w-full text-center bg-purple-700 hover:bg-purple-800 rounded-md">
+              Resume
+            </a>
           </div>
-          <div id="about-buttons" className="flex gap-4 mt-auto">
-
+          <div id="about-buttons" className="flex gap-5 mt-auto">
             <a
               href="#"
               className="w-fit h-fit rounded-full bg-zinc-800 p-4 mt-10 border border border-zinc-700"
             >
-              <FaGithub className="text-purple-200" size={20}></FaGithub>
+              <FaGithub className="text-purple-200" size={28}></FaGithub>
             </a>
 
             <a
               href="#"
               className="w-fit h-fit rounded-full bg-zinc-800 mt-10 p-4 border border border-zinc-700"
             >
-              <FaLinkedinIn className="text-purple-200" size={20}></FaLinkedinIn>
+              <FaLinkedinIn
+                className="text-purple-200"
+                size={28}
+              ></FaLinkedinIn>
             </a>
 
             <a
@@ -67,13 +87,13 @@ export default function Header() {
             >
               <FaEnvelopeOpen
                 className="text-purple-200"
-                size={20}
+                size={28}
               ></FaEnvelopeOpen>
             </a>
           </div>
 
           <div className="mt-3 px-10 text-center font-mono text-md text-zinc-400">
-            <p>Developed by Eric Dam ⌨️</p>
+            <p>Developed by Eric Dam 🍚</p>
           </div>
         </div>
       </div>
