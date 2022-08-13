0ad903fced90cdc888af9f1ce456e37c2faae2b3
Eric Dam <dam.eric3@gmail.com>
2022-08-12 22:58:58 -0700

    added mobile nav

--- components/header.js
+++ components/header.js
@@ -1,24 +1,81 @@
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaEnvelopeOpen } from "react-icons/fa";
+import { useState } from "react";
+
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const openMenu = () => {
+    let mobileMenu = document.getElementById("mobile-menu");
+    let links = document.getElementById("mobile-menu-links");
+    if (!open) {
+      mobileMenu.classList.remove("h-0");
+      mobileMenu.classList.add("h-screen");
+      // document.body.setAttribute("style", "height: 100%");
+      // document.body.getAttribute("style").add("margin: 0");
+      // links.classList.remove("hidden");
+      setOpen(true);
+    } else {
+      mobileMenu.classList.add("h-0");
+      mobileMenu.classList.remove("h-screen");
+      // links.classList.add("hidden");
+      setOpen(false);
+    }
+
+  }
+
   return (
     <div
       id="header"
-      className="h-20 z-100 backdrop-blur-sm w-full fixed bg-transparent border border-transparent border-b-zinc-800"
+      className="top-0 h-20 z-100 w-full fixed bg-zinc-900/40 border-b-2 border-b-zinc-800"
     >
-      <div className="icon p-5 absolute top-0 right-0">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-10 w-10 text-purple-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M4 6h16M4 12h16m-7 6h7"
-          />
-        </svg>
+      <div className="p-5 flex flex-col items-end backdrop-blur-lg">
+        <HiOutlineMenuAlt3 size={36} className="text-purple-200" onClick={() => openMenu()}></HiOutlineMenuAlt3>
+      </div>
+
+      <div id="mobile-menu" className="top-0 fixed z-101 text-purple-100 text-l font-semibold top-0 h-0 w-full bg-zinc-900 transition-all ease-in-out duration-300 overflow-hidden">
+        <div id="mobile-menu-links" className="h-full flex gap-3 flex-col p-5 items-center">
+          <IoMdClose size={36} className="text-purple-200 self-end mb-5" onClick={() => openMenu()}></IoMdClose>
+          <div className="flex gap-5 flex-col items-center">
+            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">About me</a>
+            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">Featured projects</a>
+            <a className="p-4 w-full text-center hover:bg-zinc-800 border border-transparent rounded-md hover:border-zinc-700">All projects</a>
+            <a className="p-4 w-full text-center bg-purple-700 hover:bg-purple-800 rounded-md">Resume</a>
+          </div>
+          <div id="about-buttons" className="flex gap-4 mt-auto">
+
+            <a
+              href="#"
+              className="w-fit h-fit rounded-full bg-zinc-800 p-4 mt-10 border border border-zinc-700"
+            >
+              <FaGithub className="text-purple-200" size={20}></FaGithub>
+            </a>
+
+            <a
+              href="#"
+              className="w-fit h-fit rounded-full bg-zinc-800 mt-10 p-4 border border border-zinc-700"
+            >
+              <FaLinkedinIn className="text-purple-200" size={20}></FaLinkedinIn>
+            </a>
+
+            <a
+              href="#"
+              className="w-fit h-fit rounded-full bg-zinc-800 p-4 mt-10 border border border-zinc-700"
+            >
+              <FaEnvelopeOpen
+                className="text-purple-200"
+                size={20}
+              ></FaEnvelopeOpen>
+            </a>
+          </div>
+
+          <div className="mt-3 px-10 text-center font-mono text-md text-zinc-400">
+            <p>Developed by Eric Dam ⌨️</p>
+          </div>
+        </div>
       </div>
     </div>
   );
